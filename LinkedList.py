aa3475eecb2e313c142ea7eb6b940b66cf19dcb9
Kamireddi Sarasa Jyothsna <54107891+Jyothsna131@users.noreply.github.com>
2020-12-14 06:55:24 +0530

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,35 @@
+# Linked List
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    def insert(self,data):
+        new_node=Node(data)
+        if not self.head:
+            self.head=new_node
+            return
+        curr=self.head
+        while curr.next:
+            curr=curr.next
+        curr.next=new_node
+
+    def print_data(self):
+        curr=self.head
+        while(curr):
+            print(curr.data)
+            curr=curr.next
+
+l1=LinkedList()
+l=[1,2,3,10,9]
+for ele in l:
+    l1.insert(ele)
+l1.print_data()
+    
+    
+        
